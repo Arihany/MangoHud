8ac612121934d8f5aa024b51c587f4d34f8ff040
Ari <82783339+Arihany@users.noreply.github.com>
2025-12-02 00:31:54 +0900

    Update android_gpu_vk_usage.h

--- src/android_gpu_vk_usage.h
+++ src/android_gpu_vk_usage.h
@@ -1,3 +1,7 @@
+// android_gpu_vk_usage.h
+
+#pragma once
+
 #if defined(__ANDROID__)
 #include <vulkan/vulkan.h>
 #include <stdint.h>
@@ -5,39 +9,44 @@
 struct AndroidVkGpuContext;
 
 struct AndroidVkGpuDispatch {
-    PFN_vkQueueSubmit          QueueSubmit;
-    PFN_vkCreateQueryPool      CreateQueryPool;
-    PFN_vkDestroyQueryPool     DestroyQueryPool;
-    PFN_vkGetQueryPoolResults  GetQueryPoolResults;
-    PFN_vkCreateCommandPool    CreateCommandPool;
-    PFN_vkDestroyCommandPool   DestroyCommandPool;
+    PFN_vkQueueSubmit            QueueSubmit;
+    PFN_vkCreateQueryPool        CreateQueryPool;
+    PFN_vkDestroyQueryPool       DestroyQueryPool;
+    PFN_vkGetQueryPoolResults    GetQueryPoolResults;
+    PFN_vkCreateCommandPool      CreateCommandPool;
+    PFN_vkDestroyCommandPool     DestroyCommandPool;
     PFN_vkAllocateCommandBuffers AllocateCommandBuffers;
-    PFN_vkFreeCommandBuffers   FreeCommandBuffers;
-    PFN_vkResetCommandBuffer   ResetCommandBuffer;
-    PFN_vkBeginCommandBuffer   BeginCommandBuffer;
-    PFN_vkEndCommandBuffer     EndCommandBuffer;
-    PFN_vkCmdWriteTimestamp    CmdWriteTimestamp;
+    PFN_vkFreeCommandBuffers     FreeCommandBuffers;
+    PFN_vkResetCommandBuffer     ResetCommandBuffer;
+    PFN_vkBeginCommandBuffer     BeginCommandBuffer;
+    PFN_vkEndCommandBuffer       EndCommandBuffer;
+    PFN_vkCmdWriteTimestamp      CmdWriteTimestamp;
 };
 
 AndroidVkGpuContext* android_gpu_usage_create(
-    VkPhysicalDevice        phys,
-    VkDevice                device,
-    float                   timestamp_period_ns,
-    const AndroidVkGpuDispatch& dispatch);
+    VkPhysicalDevice               phys,
+    VkDevice                       device,
+    float                          timestamp_period_ns,
+    const AndroidVkGpuDispatch&    dispatch);
 
 void android_gpu_usage_destroy(AndroidVkGpuContext* ctx);
 
 void android_gpu_usage_on_present(
-    AndroidVkGpuContext*    ctx,
-    VkQueue                 present_queue,
-    uint32_t                present_queue_family,
-    const VkPresentInfoKHR* pPresentInfo,
-    uint32_t                swapchain_index,
-    uint32_t                image_index);
+    AndroidVkGpuContext*           ctx,
+    VkQueue                        present_queue,
+    uint32_t                       present_queue_family,
+    const VkPresentInfoKHR*        pPresentInfo,
+    uint32_t                       swapchain_index,
+    uint32_t                       image_index);
 
 bool android_gpu_usage_get_metrics(
-    AndroidVkGpuContext*    ctx,
-    float*                  out_gpu_time_ms,
-    float*                  out_gpu_usage_percent);
+    AndroidVkGpuContext*           ctx,
+    float*                         out_gpu_time_ms,
+    float*                         out_gpu_usage_percent);
 
-#endif
+// ★ GPU 백엔드용: 컨텍스트 모르게 “마지막 값”만 들고오는 전역 헬퍼
+bool android_gpu_usage_peek_latest(
+    float* out_gpu_time_ms,
+    float* out_gpu_usage_percent);
+
+#endif // __ANDROID__
