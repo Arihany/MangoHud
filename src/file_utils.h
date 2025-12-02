9ceb0839648f8682aed57ca70fe8b44f471d10e9
Ari <82783339+Arihany@users.noreply.github.com>
2025-12-02 10:26:41 +0900

    Update file_utils.h

--- src/file_utils.h
+++ src/file_utils.h
@@ -11,17 +11,26 @@ namespace fs = ghc::filesystem;
 
 enum LS_FLAGS
 {
-    LS_DIRS = 0x01,
+    LS_DIRS  = 0x01,
     LS_FILES = 0x02,
 };
 
 std::string read_line(const std::string& filename);
-std::vector<std::string> ls(const char* root, const char* prefix = nullptr, LS_FLAGS flags = LS_DIRS);
+
+std::vector<std::string> ls(const char* root,
+                            const char* prefix = nullptr,
+                            LS_FLAGS flags = LS_DIRS);
+
 bool file_exists(const std::string& path);
 bool dir_exists(const std::string& path);
-std::string read_symlink(const char * link);
-std::string read_symlink(const std::string&& link);
-std::string get_basename(const std::string&& path); //prefix so it doesn't conflict libgen
+
+std::string read_symlink(const char* link);
+// ★ rvalue 말고 ref로 통일
+std::string read_symlink(const std::string& link);
+
+// ★ 이것도 ref로 통일
+std::string get_basename(const std::string& path); // name만 basename과 안 겹치게 한 거지, rvalue랑 아무 상관 없음
+
 std::string get_exe_path();
 std::string get_wine_exe_name(bool keep_ext = false);
 std::string get_home_dir();
@@ -31,4 +40,4 @@ bool lib_loaded(const std::string& lib, pid_t pid);
 std::string remove_parentheses(const std::string&);
 std::string to_lower(const std::string& str);
 
-#endif //MANGOHUD_FILE_UTILS_H
+#endif // MANGOHUD_FILE_UTILS_H
