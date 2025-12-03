7730ddf4228d2e18ea4aa1f6c481485f18a7f4e3
Ari <82783339+Arihany@users.noreply.github.com>
2025-12-03 15:41:44 +0900

    Update battery.h

--- src/battery.h
+++ src/battery.h
@@ -1,26 +1,34 @@
 #pragma once
+
 #include <string>
+#include <vector>
 
 #ifndef MAX_BATTERY_COUNT
 #define MAX_BATTERY_COUNT 4
 #endif
 
-class BatteryStats{
-    public:
-        void numBattery();
-        void update();
-        float getPower();
-        float getPercent();
-        float getTimeRemaining();
-        std::string battPath[2];
-        float current_watt = 0;
-        float current_percent = 0;
-        float remaining_time = 0;
-        std::string current_status;
-        std::string state [2];
-        int batt_count=0;
-        bool batt_check = false;
-        std::vector<float> current_now_vec = {};
+class BatteryStats {
+public:
+    void numBattery();
+    void update();
+
+    float getPower();
+    float getPercent();
+    float getTimeRemaining();
+
+    std::string battPath[MAX_BATTERY_COUNT];
+    std::string state[MAX_BATTERY_COUNT];
+
+    float current_watt    = 0.0f;
+    float current_percent = 0.0f;
+    float remaining_time  = 0.0f;
+
+    std::string current_status;
+
+    int  batt_count  = 0;
+    bool batt_check  = false;
+
+    std::vector<float> current_now_vec{};
 };
 
 extern BatteryStats Battery_Stats;
